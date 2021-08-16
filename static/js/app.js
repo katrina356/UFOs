eee957dd9ec6bf377fa4a5678a9221a3328f2003
Katrina Holcomb <katrina504@msn.com>
2021-08-15 18:12:14 -0600

    final app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -19,4 +19,28 @@ function buildTable(data) {
                 cell.text(val);
             });
     });
-}
+};
+
+// start new function called handleClick
+function handleClick() {
+    //select the 1st element that matches datetime, chaining .property tells js to grab and hold in the date variable
+    let date = d3.select("#datetime").property("value");
+    // set default filter. tableData is our original file so if no date is picked then all data will be returned
+    let filteredData = tableData;
+    // Check to see if a date was entered and filter the data using that date
+    if (date) {
+        //this applies the filter to the data to the date variable created above
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //Rebuild the table using the filtered data
+    // @Note:  If no date was entered, then filteredData will just be the original tableData.
+    buildTable(filteredData);
+};
+// Selector string #filter-btn is a unique id to a button element in the HTML called filter-btn, this links our code directly to the filter button
+// .on(click, handleClick) tells d3 to execute our handleClick function
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
